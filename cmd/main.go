b4eb19fd36bd526fba0e2ed921b20f44e0c11489
shakezula <lott.dylan@gmail.com>
2024-05-14 23:43:18 -0600

    adjusts file names and cleanup

--- cmd/main.go
+++ cmd/main.go
@@ -1,43 +1,42 @@
 package main
 
 import (
-	"context"
+	"fmt"
 	"time"
 
-	"oberon/pkg/game"
+	"oberon/pkg/entity"
 )
 
 func main() {
-	config := game.Config{}
-	looper, _ := game.New(config)
+	world := entity.NewWorld()
+	tickDuration := time.Second // 1 second per tick
 
-	initialState := []game.Observer{
-		&game.Player{
-			Username: "shakezula",
-			State:    map[string]interface{}{},
-		},
-		&game.Player{
-			Username: "meatwad",
-			State:    map[string]interface{}{},
-		},
-		&game.Player{
-			Username: "frylock",
-			State:    map[string]interface{}{},
-		},
-		&game.Player{
-			Username: "carl",
-			State:    map[string]interface{}{},
-		},
-	}
+	entityID := world.NewEntity()
 
-	// register everything
-	for _, item := range initialState {
-		looper.Register(item)
-	}
+	world.AddResource(entityID, entity.Resource{Name: "Gold", Count: 0})
+	world.AddShip(entityID, entity.Ship{
+		Name:    "Karakoum",
+		OwnerID: "shakezula",
+		Cargo: []entity.Resource{
+			{Name: "Gold", Count: 1},
+		},
+	})
+	world.AddStarSystem(entityID, entity.StarSystem{Name: "Alpha Centauri"})
 
-	ctx := context.Background()
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
+	ticker := time.NewTicker(tickDuration)
+	defer ticker.Stop()
+	done := make(chan bool)
 
-	looper.Run(ctx, time.Second*1)
+	go func(world *entity.World, ticker *time.Ticker, done chan bool) {
+		for {
+			select {
+			case <-done:
+				fmt.Printf("initiating graceful shutdown...")
+				return
+			case t := <-ticker.C:
+				world.Update(1.0) // Update the world with a delta time of 1.0
+				fmt.Printf("Tick at %v\n", t)
+			}
+		}
+	}(world, ticker, done)
 }
